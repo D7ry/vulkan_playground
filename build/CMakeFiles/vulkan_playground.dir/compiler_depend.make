947f8d3512a7ebb8f6b7e04d8085bfbed6627e07
d7ryLinux <yty108102@gmail.com>
2023-12-15 23:15:11 -0800

    initialize

---
+++ build/CMakeFiles/vulkan_playground.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for vulkan_playground.
+# This may be replaced when dependencies are built.
---
+++ src/application/vulkan_application.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <vulkan/vulkan.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <algorithm>
+#include <cstddef>
+#include <cstdint>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <limits>
+#include <optional>
+#include <set>
+#include <stdexcept>
+#include <vector>
