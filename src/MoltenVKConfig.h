f32e45a11590e15e500b1a8d22771c801135ebf0
dtry <7ianyun@berkeley.edu>
2024-09-09 19:53:52 -0700

    exiting moltenVK segfault city

--- src/MoltenVKConfig.h
+++ src/MoltenVKConfig.h
@@ -16,14 +16,14 @@ inline VkLayerSettingsCreateInfoEXT GetLayerSettingsCreatInfo() {
     VkLayerSettingsCreateInfoEXT layerSettingsCreateInfo = {};
     layerSettingsCreateInfo.sType
         = VK_STRUCTURE_TYPE_LAYER_SETTINGS_CREATE_INFO_EXT;
-    std::vector<VkLayerSettingEXT> settings;
+    static std::vector<VkLayerSettingEXT> settings;
     VkLayerSettingEXT traceSetting = {};
     traceSetting.pLayerName = "MoltenVK";
     traceSetting.pSettingName = "MVK_CONFIG_USE_METAL_ARGUMENT_BUFFERS";
     traceSetting.valueCount = 1;
     traceSetting.type = VK_LAYER_SETTING_TYPE_BOOL32_EXT;
-    uint32_t value = true;
-    traceSetting.pValues = &value;
+    static uint32_t value = true;
+    traceSetting.pValues = std::addressof(value);
     settings.push_back(traceSetting);
 
     layerSettingsCreateInfo.settingCount
