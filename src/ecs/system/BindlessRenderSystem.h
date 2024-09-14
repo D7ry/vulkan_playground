473327106684aa8f5593e1987eca69f010a64c57
d7ryLinux <yty108102@gmail.com>
2024-09-13 18:16:24 -0700

    clean up bindless system header

--- src/ecs/system/BindlessRenderSystem.h
+++ src/ecs/system/BindlessRenderSystem.h
@@ -14,13 +14,71 @@
 // as opposed to O(# of unique mesh instance), or O(# of unique mesh model)
 class BindlessRenderSystem : public IRenderSystem
 {
-    /* ---------- SSBO Data Types ---------- */
+  public:
+    virtual void Init(const InitContext* initData) override;
+    virtual void Tick(const TickContext* tickData) override;
+    virtual void Cleanup() override;
 
+    // TODO: is this a good abstraction barrier?
+    BindlessRenderSystemComponent* MakeComponent(
+        const std::string& meshPath,
+        const std::string& texturePath
+    );
+    void DestroyComponent(BindlessRenderSystemComponent*& component);
+
+    // flag the entity as dirty, so that its buffer will be flushed
+    void FlagUpdate(Entity* entity);
+
+  private:
+    /* ---------- Graphics Pipeline ---------- */
+    enum class BindingLocation : unsigned int
+    {
+        UBO_STATIC_ENGINE = 0,
+        INSTANCE_DATA = 1,
+        INSTANCE_INDEX = 2,
+        TEXTURE_SAMPLER = 3
+    };
+    const char* VERTEX_SHADER_SRC = "../shaders/bindless.vert.spv";
+    const char* FRAGMENT_SHADER_SRC = "../shaders/bindless.frag.spv";
+
+    // pipeline
+    VkPipeline _pipeline = VK_NULL_HANDLE;
+    VkPipelineLayout _pipelineLayout = VK_NULL_HANDLE;
+
+    // sysetm holds its own descriptor pool
+    VkDescriptorSetLayout _descriptorSetLayout = VK_NULL_HANDLE;
+    VkDescriptorPool _descriptorPool = VK_NULL_HANDLE;
+
+    std::array<VkDescriptorSet, NUM_FRAME_IN_FLIGHT> _descriptorSets;
+
+    VQDevice* _device = nullptr;
+
+    /* ---------- Texture Resources ---------- */
+    TextureManager* _textureManager;
+
+    // an array of texture descriptors that gets filled up as textures are
+    // loaded in
+    std::array<VkDescriptorImageInfo, TEXTURE_ARRAY_SIZE>
+        _textureDescriptorInfo;
+    size_t _textureDescriptorInfoIdx
+        = 0; // idx to the current texture descriptor that can be written in,
+             // also represents the total # of valid texture descriptors,
+             // starting from the beginning of the _textureDescriptorInfo array
+    std::unordered_map<std::string, int>
+        _textureDescriptorIndices; // texture name, index into the
+                                   // texture descriptor array
+
+    /* ---------- SSBO Data Types ---------- */
     // https://docs.vulkan.org/guide/latest/shader_memory_layout.html
-    static const unsigned int SSBO_INSTANCE_DATA_ALIGNMENT = 16; // OpDecorate %array140 ArrayStride 16
-    static const unsigned int SSBO_INSTANCE_INDEX_ALIGNMENT = 4; // OpDecorate %array430 ArrayStride 4
+    static const unsigned int SSBO_INSTANCE_DATA_ALIGNMENT
+        = 16; // OpDecorate %array140 ArrayStride 16
+    static const unsigned int SSBO_INSTANCE_INDEX_ALIGNMENT
+        = 4; // OpDecorate %array430 ArrayStride 4
     using SSBOInstanceIndex = unsigned int;
-    static_assert(sizeof(SSBOInstanceIndex) % SSBO_INSTANCE_INDEX_ALIGNMENT == 0);
+    static_assert(
+        sizeof(SSBOInstanceIndex) % SSBO_INSTANCE_INDEX_ALIGNMENT == 0
+    );
+
     // data structure per bindless system instance
     // lives on the `instanceDataArray` buffer
     struct SSBOInstanceData
@@ -38,13 +96,14 @@ class BindlessRenderSystem : public IRenderSystem
 
         const uint32_t padding[1];
     };
-    static_assert(sizeof(SSBOInstanceData) % SSBO_INSTANCE_DATA_ALIGNMENT == 0);
 
+    static_assert(sizeof(SSBOInstanceData) % SSBO_INSTANCE_DATA_ALIGNMENT == 0);
 
     // note that we don't create NUM_FRAME_IN_FLIGHT vertex/index
     // buffers assuming synchronization is trivial
     // TODO: add synchronization protection to them.
 
+    /* ---------- Bindless Device Resources ---------- */
     // all index buffers
     VQBuffer _indexBuffers;
     unsigned int _indexBuffersWriteOffset = 0;
@@ -76,8 +135,7 @@ class BindlessRenderSystem : public IRenderSystem
              // VkDrawIndexedIndirectCommand
     unsigned int _instanceDataArrayOffset = 0;
 
-    // maps model to a vec of batches that renders the model
-
+    /* ---------- Internal Data Structures ----------- */
     struct RenderBatch
     {
         unsigned int maxSize;
@@ -87,81 +145,30 @@ class BindlessRenderSystem : public IRenderSystem
         // additional render batch infos
     };
 
+    // each model can have multiple batches, with each batch being able to 
+    // render more instances than the previous one. This avoids fragmentation
+    // and reduces model resizing cost. Note this does create a manageable amount of 
+    // additional draw calls for large instance count, but if we scale up batch size
+    // geometrically the draw call cost can be treated as O(1).
     std::unordered_map<std::string, std::vector<RenderBatch>> _modelBatches;
 
-  public:
+    DeletionStack _deletionStack;
+
+    // list of functions that updates each frame in flight
+    std::array<std::vector<std::function<void()>>, NUM_FRAME_IN_FLIGHT>
+        _updateQueue;
+
+    /* ---------- Private Methods ----------- */
     RenderBatch createRenderBatch(
         const std::string& meshPath,
         unsigned int batchSize
     );
-    BindlessRenderSystemComponent* MakeComponent(
-        const std::string& meshPath,
-        const std::string& texturePath
-    );
-
-    void DestroyComponent(BindlessRenderSystemComponent*& component);
-
-    virtual void Init(const InitContext* initData) override;
-    virtual void Tick(const TickContext* tickData) override;
-
-    virtual void Cleanup() override;
-
-    // flag the entity as dirty, so that its buffer will be flushed
-    void FlagUpdate(Entity* entity);
-
-  private:
-    void updateMeshInstanceData(int frame);
-    // spir-v source to vertex and fragment shader, relative to compiled binary
-    const char* VERTEX_SHADER_SRC = "../shaders/bindless.vert.spv";
-    const char* FRAGMENT_SHADER_SRC = "../shaders/bindless.frag.spv";
-
-    // list of functions that updates each frame in flight
-    std::array<std::vector<std::function<void()>>, NUM_FRAME_IN_FLIGHT> _updateQueue;
-
-    // pipeline
-    VkPipeline _pipeline = VK_NULL_HANDLE;
-    VkPipelineLayout _pipelineLayout = VK_NULL_HANDLE;
-
-    // sysetm holds its own descriptor pool
-    VkDescriptorSetLayout _descriptorSetLayout = VK_NULL_HANDLE;
-    VkDescriptorPool _descriptorPool = VK_NULL_HANDLE;
-
-    std::array<VkDescriptorSet, NUM_FRAME_IN_FLIGHT> _descriptorSets;
-
-    VQDevice* _device = nullptr;
-
-    // initialize resources for graphics pipeline,
-    // - shaders
-    // - descriptors(pool, layout, sets)
-    // and the pipeline itself
     void createGraphicsPipeline(
         const VkRenderPass renderPass,
         const InitContext* initData
     );
-
-    enum class BindingLocation : unsigned int
-    {
-        UBO_STATIC_ENGINE = 0,
-        INSTANCE_DATA = 1,
-        INSTANCE_INDEX = 2,
-        TEXTURE_SAMPLER = 3
-    };
-
-    TextureManager* _textureManager;
-
-    // an array of texture descriptors that gets filled up as textures are
-    // loaded in
-    std::array<VkDescriptorImageInfo, TEXTURE_ARRAY_SIZE>
-        _textureDescriptorInfo;
-    size_t _textureDescriptorInfoIdx
-        = 0; // idx to the current texture descriptor that can be written in,
-             // also represents the total # of valid texture descriptors,
-             // starting from the beginning of the _textureDescriptorInfo array
-    std::unordered_map<std::string, int>
-        _textureDescriptorIndices; // texture name, index into the
-                                   // texture descriptor array
-    
-    // flush the `_textureDescriptorInfo` into device, updating the descriptor set
+    // flush the `_textureDescriptorInfo` into device, updating the descriptor
+    // set
     void updateTextureDescriptorSet(int frame);
 
     // create resrouces required for bindless rendering. Including:
@@ -169,6 +176,4 @@ class BindlessRenderSystem : public IRenderSystem
     // - a less huge SSBO to store pointers to all instance data
     // - a UBO to store parameters of draw commands
     void createBindlessResources();
-
-    DeletionStack _deletionStack;
 };
