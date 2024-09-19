0824c15160ac22355f4e553459e1bf3418ce7d49
dtry <7ianyun@berkeley.edu>
2024-09-18 22:25:38 -0700

    feat: monitor device info

--- src/components/imgui_widgets/ImGuiWidgetDeviceInfo.cpp
+++ src/components/imgui_widgets/ImGuiWidgetDeviceInfo.cpp
@@ -4,14 +4,32 @@
 
 // current setup assumes single logical device
 void ImGuiWidgetDeviceInfo::Draw(const VulkanEngine* engine) {
-    VQDevice* device = engine->_device.get();
-    auto properties = device->properties;
-    ImGui::SeparatorText("General");
-    ImGui::Text("%s", device->properties.deviceName);
-    ImGui::SeparatorText("Device Extensions");
-    ImGui::Indent(10);
-    for (const char* extension : engine->DEVICE_EXTENSIONS) {
-        ImGui::Text("%s", extension);
+    const int INDENT = 20;
+    { // GPU
+        ImGui::SeparatorText("GPU");
+        VQDevice* device = engine->_device.get();
+        auto properties = device->properties;
+        ImGui::Text("Device: %s", device->properties.deviceName);
+        ImGui::Text("Device Extensions: ");
+        ImGui::Indent(INDENT);
+        for (const char* extension : engine->DEVICE_EXTENSIONS) {
+            ImGui::Text("%s", extension);
+        }
+        ImGui::Indent(-INDENT);
     }
-    ImGui::Indent(-10);
+    { // Display
+        ImGui::SeparatorText("Display");
+        GLFWmonitor* monitor = glfwGetWindowMonitor(engine->_window);
+        if (monitor) {
+            ImGui::Text("Device: %s", glfwGetMonitorName(monitor));
+            const GLFWvidmode* mode = glfwGetVideoMode(monitor);
+            if (mode) {
+                ImGui::Text("Size: %i x %i", mode->width, mode->height);
+                ImGui::Text("Refresh Rate: %i hz", mode->refreshRate);
+            }
+        } else {
+            ImGui::Text("Application running in windowed mode, no dedicated "
+                        "display is used.");
+        }
+    }
 }
